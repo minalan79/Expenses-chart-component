d0e5dcc173f473c6efa9e4d9e4d2074087779c6f
Minalan <minalan79@gmail.com>
2023-03-28 05:13:15 +0000

    Update Graph.jsx

--- src/Graph.jsx
+++ src/Graph.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -7,9 +7,9 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-import json from './assets/images/data.json'
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import json from "./assets/images/data.json";
 
 ChartJS.register(
   CategoryScale,
@@ -24,55 +24,58 @@ export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: 'top',
+      position: "top",
     },
     title: {
       display: true,
-      text: 'Chart.js Bar Chart',
+      text: "Chart.js Bar Chart",
     },
-    scales: {
-      x: {
+  },
+  scales: {
+    y: [
+      {
         grid: {
+          color: "#aaa",
+          borderDash: [1, 3],
           display: false,
-          lineWidth: 0,
-        }
+        },
+        display: false, // this will hide vertical lines
       },
-      y: {
+    ],
+    xAxes: [
+      {
         grid: {
+          color: "#aaa",
+          borderDash: [1, 3],
           display: false,
-          lineWidth: 0,
-        }
+        },
+        display: false, // this will hide vertical lines
       },
-    },
+    ],
   },
 };
 
-const labels = json.map(j => j.day);
+const labels = json.map((j) => j.day);
 
 export const data = {
   labels,
   datasets: [
     {
-      label: 'Dataset 1',
-      data: json.map(j => j.amount),
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      xAxisID: "xAxes",
+      label: "Dataset 1",
+      data: json.map((j) => j.amount),
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
   ],
 };
-
 
 function Graph() {
   console.log(labels, data);
   return (
     <>
-      <Bar 
-        data= {data}
-        height={400}
-        width={600}
-        options={options}
-      />
+      <Bar data={data} height={400} width={600} options={options} />
     </>
-  )
+  );
 }
 
 export default Graph;
